1c05c1bc0d2357aec2663f88dba606c01116a6ad
unknown <pan_rostislav@ukr.net>
2019-05-03 12:22:18 +0300

    restyle home page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
-    <v-toolbar class="menu">
+    <v-toolbar class="menu white--text" color="#512DA8">
       <v-toolbar-title class="headline text-uppercase">
-      <span class="grey--text text--darken-4">Cinema</span>
-      <span class="indigo--text text--darken-2 font-weight-light">VUETIFUL APP</span>
+      <span class="my-gr">Cinema</span>
+      <span class="font-weight-light">VUETIFUL APP</span>
     </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn to="/" flat>Home</v-btn>
-        <v-btn to="/films" flat>Films</v-btn>
-        <v-btn to="/about" flat>About</v-btn>
+        <v-btn class="white--text" to="/" flat>Home</v-btn>
+        <v-btn class="white--text" to="/films" flat>Films</v-btn>
+        <v-btn class="white--text" to="/about" flat>About</v-btn>
       </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -25,5 +25,13 @@
 </script>
 
 <style scoped>
-
+  .menu {
+    color: white;
+  }
+  .font-weight-light {
+    /* color: #4CAF50; */
+  }
+  .my-gr {
+    color:#61B965;
+  }
 </style>
