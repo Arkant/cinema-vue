c9b08941e6349f98c9a4469311362df1017c57a4
unknown <pan_rostislav@ukr.net>
2019-05-14 16:25:52 +0300

    fix lazy loading router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import SignUp from './views/Sign-Up.vue';
 Vue.use(Router)
 
 export default new Router({
