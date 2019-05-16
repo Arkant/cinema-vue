59e029c1dac35f3924060a485d0ca7a9ffa32c2f
unknown <pan_rostislav@ukr.net>
2019-05-16 17:31:54 +0300

    managed to fetch data from firestore

--- src/store/modules/films.module.js
+++ src/store/modules/films.module.js
@@ -1,8 +1,28 @@
-const films = {
-  state: { },
-  mutations: {  },
-  actions: {  },
-  getters: {  }
+// import firebase from 'firebase';
+// import router from '@/router'
+import { db } from '../../main';
+
+const filmsModule = {
+  state: {
+    films: []
+  },
+  mutations: { 
+    setAllFilms(state, payload) {
+      payload.forEach(doc => {
+        state.films.push(doc.data());
+      })
+    }
+  },
+  actions: { 
+    getAllFilms({commit}) {
+      db.collection('films').get().then((snapshot) => {
+        commit('setAllFilms', snapshot.docs);
+      });
+    }
+  },
+  getters: { 
+    getAllFilms : state => state.films
+  }
 }
 
-export default films;
+export default filmsModule;
--- src/views/Films.vue
+++ src/views/Films.vue
@@ -1,19 +1,42 @@
 <template>
 <v-container>
   it's Films
+  {{getAllFilms}}
+  <!-- <div v-for="(user, idx) in users" :key="idx">
+    <h1>{{ user.name }}</h1>
+ </div> -->
 </v-container>
 </template>
 
 <script>
 import router from '@/router';
+import { mapGetters } from 'vuex';
+
 
 export default {
-  components: {
+  data() {
+    return {
+      films: [],
+      name: 'ihor',
+      users: null
+    }
   },
   mounted() {
-    if(!this.$store.getters.isAuthenticated) {
+    if (!this.$store.getters.isAuthenticated) {
       router.push('/');
+    } else {
+      this.getRecipes();
     }
+  },
+  methods: {
+    getRecipes() {
+      this.$store.dispatch('getAllFilms');
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getAllFilms'
+    ])
   }
 }
 </script>
